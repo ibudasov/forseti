63f51186a04ce97a47e37ef7f7b01ef5cc18a614
Igor Budasov <igor.budasov@gmail.com>
2026-08-05 12:45:58 +0200

    refactor: clean up main.py by removing unused Item class and updating welcome message

--- app/main.py
+++ app/main.py
@@ -1,24 +1,9 @@
 from dataclasses import asdict
-
 from fastapi import FastAPI
 from pedantic import frozen_dataclass
 
-
-@frozen_dataclass(type_safe=True)
-class Item:
-    name: str
-    price: float
-    quantity: int = 1
-
-
 app = FastAPI(title="Forseti API")
-
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Forseti FastAPI"}
-
-
-@app.post("/items/")
-def create_item(item: Item):
-    return {"item": asdict(item)}
+    return {"message": "Welcome to Forseti API!"}
